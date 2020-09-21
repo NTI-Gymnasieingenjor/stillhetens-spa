8f7fc830a4e6a9b434068c31f618eeba767fc738
Erik Reider <erik.reider@elev.ga.ntig.se>
2020-09-21 14:38:04 +0200

    Selenium tests CI

--- tests/webtests/BaseTest.py
+++ tests/webtests/BaseTest.py
@@ -10,7 +10,10 @@ else:
 
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
-driver = webdriver.Chrome(executable_path=(os.getcwd() + "/../chromedriver" + exe), options=options)
-driver.get("google.com")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+driver = webdriver.Chrome(executable_path=(os.getcwd() + "/../chromedriver" + exe), chrome_options=options)
+driver.get("https://www.google.com/")
 
 assert driver.find_element_by_id("hplogo")
+print(driver.find_element_by_id("hplogo").get_attribute("src"))
