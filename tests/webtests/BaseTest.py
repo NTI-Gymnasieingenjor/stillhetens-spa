cd89b4cfd65f30453951febee1fe20f72743b3fb
Erik Reider <erik.reider@elev.ga.ntig.se>
2020-09-21 15:37:37 +0200

    Everything works

--- tests/webtests/BaseTest.py
+++ tests/webtests/BaseTest.py
@@ -13,7 +13,3 @@ options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(executable_path=(os.getcwd() + "/../chromedriver" + exe), chrome_options=options)
-driver.get("https://fantastic4group.gitlab.io/pizza-website/")
-
-assert driver.find_element_by_id("titleTextTest") == "eeee"
-print(driver.find_element_by_id("titleTextTest"))
