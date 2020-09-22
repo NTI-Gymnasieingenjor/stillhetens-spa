fda2bd88838e07bc6de299980a6361d489104d9a
Albin Nyman <joshei@telia.com>
2020-09-22 13:13:17 +0000

    Update BaseTest.py

--- tests/webtests/BaseTest.py
+++ tests/webtests/BaseTest.py
@@ -9,7 +9,7 @@ else:
     exe=".exe"
 
 options = webdriver.ChromeOptions()
-#options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(executable_path=(os.getcwd() + "/../chromedriver" + exe), chrome_options=options)
