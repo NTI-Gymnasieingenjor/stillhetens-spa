b12d142e051bd17f7a7e88c8d84df6eac898482d
Erik Reider <erik.reider@elev.ga.ntig.se>
2020-09-23 11:19:49 +0200

    Added comments to the css validator and made the HTML validator more inline with the css one

--- tests/Validation/CSSValidator.py
+++ tests/Validation/CSSValidator.py
@@ -4,23 +4,55 @@ import os
 import json
 from colorama import Fore, Back, Style
 
-def Loops(type):
-    for i in list[type]:
-        print(i)
+# The variable that turns to true when there's an error
+global exitVar
+exitVar = False
+
+
+def printMessage(type: str, filePath: str):
+    # For-loop that prints out all the errors and warnings in the "output" list
+    for i in output[type]:
+        print("---------------------------------------------------------------------")
+        if type == "errors":
+            print(Back.RED + "ERROR" + Style.RESET_ALL)
+        elif type == "warnings":
+            print(Back.YELLOW + Fore.BLACK + "WARNING" + Style.RESET_ALL)
+        print("Fil:", filePath.replace("../../", ""))
+        print("Radnummer:", i["line"])
+        print("Meddelande:", i)
+
 
 def sendFileAndPrint(filePath: str):
-    request = json.loads(os.popen('curl -s -H "Content-Type: multipart/form-data" -F "text=<' + filePath + ';type=text/plain" -F "output=json" https://jigsaw.w3.org/css-validator/validator').read())
-    global list 
-    list = request["cssvalidation"]
-    if list["result"]["errorcount"] > 0:
-        print(Back.RED + "ERROR" + Style.RESET_ALL)
-        Loops("errors")
+    # Sets the variable output as a global variable
+    # It runs the curl command in terminal and then converts the resulting json to a python dictionary
+    global output
+    output = json.loads(os.popen('curl -s -H "Content-Type: multipart/form-data" -F "text=<' + filePath +
+                                 ';type=text/plain" -F "output=json" https://jigsaw.w3.org/css-validator/validator').read())["cssvalidation"]
 
-    if list["result"]["warningcount"] > 0:
-        print(Back.YELLOW + Fore.BLACK + "WARNING" + Style.RESET_ALL)
-        Loops("warnings")
-    if list["result"]["warningcount"] == 0 and list["result"]["errorcount"] == 0:
-        print(Back.GREEN + Fore.BLACK + "Validation succeded" + Style.RESET_ALL)
-    
+    # Creates the count variables
+    errorCount = output["result"]["errorcount"]
+    warningCount = output["result"]["warningcount"]
+
+    # When there are errors
+    if errorCount > 0:
+        global exitVar
+        exitVar = True
+        printMessage("errors", filePath)
+    # When there are warnings
+    if warningCount > 0:
+        printMessage("warnings", filePath)
+
+    # When there aren't any errors or warnings
+    if errorCount == 0 and warningCount == 0:
+        print(Back.GREEN + Fore.BLACK + "Validation succeeded" + Style.RESET_ALL)
+
+    print("---------------------------------------------------------------------")
+
+# Looks for all the CSS files in the public/assets/less/dist directory to send to the validator
 for file in glob.glob("../../public/assets/less/dist/*.css"):
     sendFileAndPrint(file)
+
+# If error message occurs, validation fails
+if exitVar == True:
+    print(Back.RED + "Validation Failed" + Style.RESET_ALL)
+    exit(1)
--- tests/Validation/HTMLValidator.py
+++ tests/Validation/HTMLValidator.py
@@ -4,7 +4,9 @@ import glob
 from colorama import Fore, Back, Style
 
 # The variable that turns to true when there's an error
+global exitVar
 exitVar = False
+
 
 def sendFileAndPrint(filePath: str):
     # Creates a variable that opens the HTML file and stores the data in binary format
@@ -12,17 +14,19 @@ def sendFileAndPrint(filePath: str):
     # Specifies to the validator that the content that is being sent to it is a HTML file
     headers = {'Content-Type': 'text/html'}
     # Sends a request to the validator that includes the content in the HTML file
-    req = requests.post(url='http://validator.w3.org/nu/?out=json', data=htmlFile, headers=headers)
+    req = requests.post(
+        url='http://validator.w3.org/nu/?out=json', data=htmlFile, headers=headers)
     # Variable that stores the output that is sent back from the validator
     # "messages" is a list that stores all the errors and warnings sent back from the validator
     output = req.json()["messages"]
     # If the "messages" list is empty, the validation succeeds
     if len(output) == 0:
-        print("Validation Succeded")
+        print(Back.GREEN + Fore.BLACK + "Validation succeeded" + Style.RESET_ALL)
         pass
     else:
         # For-loop that prints out all the errors and warnings in the "messages" list
         for i in range(len(output)):
+            global exitVar
             exitVar = True
             messages = output[i]
             print("--------------------")
@@ -32,14 +36,17 @@ def sendFileAndPrint(filePath: str):
             # Color scheme for warning messages
             if messages["type"] == "info":
                 print(Back.YELLOW + Fore.BLACK + "WARNING" + Style.RESET_ALL)
-            print("Fil:", filePath)
-            print("Radnummer:",messages["lastLine"])
+            print("Fil:", filePath.replace("../../", ""))
+            print("Radnummer:", messages["lastLine"])
             print("Meddelande:", messages["message"])
             print("--------------------")
+
+
 # Looks for all the HTML files in the public directory to send to the validator
 for file in glob.glob("../../public/*.html"):
     sendFileAndPrint(file)
-    
-# If error message occurs, validation fails 
+
+# If error or warning message occurs, validation fails
 if exitVar == True:
+    print(Back.RED + "Validation Failed" + Style.RESET_ALL)
     exit(1)
