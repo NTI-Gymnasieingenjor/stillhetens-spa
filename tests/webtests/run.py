4f345e308b150b5bd43d1b99bead0e5bad61af07
Erik Reider <erik.reider@elev.ga.ntig.se>
2020-09-24 15:33:18 +0200

    Fixed the run.py test to terminate the server without killing python

--- tests/webtests/run.py
+++ tests/webtests/run.py
@@ -1,29 +1,36 @@
 import os
-from selenium import webdriver 
+from selenium import webdriver
 import argparse
 from sys import platform
 import subprocess
-from selenium.common.exceptions import NoSuchElementException
+from atexit import register
 
 from infoTest import InfoTest
 
-# Checks if the test is run on Linux or Windows, 
+# Checks if the test is run on Linux or Windows,
 # and chooses driver accordingly
 if platform == "linux":
-    exe=""
+    exe = ""
 else:
-    exe=".exe"
+    exe = ".exe"
 
-# Kills the previous server incase it didn't close last session
-os.system("killall -9 python3")
+
 # Opens a localhost server with the port 6969 and in the public directory
-server = subprocess.Popen(["python3", "-m", "http.server",  "6969"], cwd="../../public", stdout=open(os.devnull, "w"), stderr=open(os.devnull, "w"))
+server = subprocess.Popen(["python3", "-m", "http.server",  "6969"],
+                          cwd="../../public", stdout=open(os.devnull, "w"), stderr=open(os.devnull, "w"))
+
+# Closes the server at the end
+def exitFunction():
+    server.terminate()
+    print("Test Finished")
+
+register(exitFunction)
 
 # Creates the variable for ChromeOptions()
 options = webdriver.ChromeOptions()
 # Makes the test run in the terminal instead of the browser
 options.add_argument("--headless")
-# Gives Chrome access to the rest of the system 
+# Gives Chrome access to the rest of the system
 options.add_argument("--no-sandbox")
 # Writes shared memory files into /tmp instead of /dev/shm
 options.add_argument("--disable-dev-shm-usage")
@@ -35,6 +42,3 @@ driver.get("http://localhost:6969/index.html")
 print("")
 
 InfoTest(driver)
-
-# Closes the server
-server.terminate()
