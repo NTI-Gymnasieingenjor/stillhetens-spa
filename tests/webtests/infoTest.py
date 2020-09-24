537205b88e2c5d2fd895f297daf70f446bab699d
Erik Reider <erik.reider@elev.ga.ntig.se>
2020-09-24 08:52:16 +0000

    Tests

--- tests/webtests/infoTest.py
+++ tests/webtests/infoTest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 class InfoTest:
     def __init__(self, driver: webdriver.Chrome):
         
-        title = driver.find_element_by_id("title")
+        title = driver.find_element_by_id("title").text
 
         assert "Stillhetens Spa" in title 
         print(title)
