0811ef135ed768bec1ad094ff306c58c1fa3e904
Erik Reider <erik.reider@elev.ga.ntig.se>
2020-09-28 09:16:49 +0000

    Fixed small changes from backlog like comments, unused imports, Spa => spa and the initial openDays test

--- tests/webtests/run.py
+++ tests/webtests/run.py
@@ -1,12 +1,12 @@
 import os
 from selenium import webdriver
-import argparse
 from sys import platform
 import subprocess
 from atexit import register
 
 from infoTest import InfoTest
 from socialmediaTest import SocialMediaTest
+from openDaysTest import OpenDaysTest
 
 # Checks if the test is run on Linux or Windows,
 # and chooses driver accordingly
@@ -33,16 +33,14 @@ register(exitFunction)
 options = webdriver.ChromeOptions()
 # Makes the test run in the terminal instead of the browser
 options.add_argument("--headless")
-# Gives Chrome access to the rest of the system
-options.add_argument("--no-sandbox")
-# Writes shared memory files into /tmp instead of /dev/shm
-options.add_argument("--disable-dev-shm-usage")
 # Gets the driver and defines its options
-driver = webdriver.Chrome(executable_path=(os.getcwd() + "/../chromedriver" + exe), chrome_options=options)
-
+driver = webdriver.Chrome(executable_path=(os.getcwd() + "/../chromedriver" + exe), options=options)
+# Navigates to the localhost, which is the site tested
 driver.get("http://localhost:6969/index.html")
 
 print("")
 
+# Runs all of the tests
 InfoTest(driver)
 SocialMediaTest(driver)
+OpenDaysTest(driver)
